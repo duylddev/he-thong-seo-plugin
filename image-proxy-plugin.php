9e901c26b92c43faa557aa167d61b68401aa3daf
duyld <duyld.dev@gmail.com>
2025-03-29 23:29:10 +0700

    Update Docker configuration and add plugin files for Image Proxy Plugin
    - Modified `docker-compose.yaml` to mount the entire project directory for plugin development.
    - Added `updater.php` for GitHub updater functionality.
    - Created `README.md` with detailed plugin features, installation, and usage instructions.

--- image-proxy-plugin.php
+++ image-proxy-plugin.php
@@ -11,6 +11,37 @@ if (!defined('ABSPATH')) {
     exit;
 }
 
+add_action('init', 'github_plugin_updater_test_init');
+function github_plugin_updater_test_init()
+{
+
+    include_once 'updater.php';
+
+    define('WP_GITHUB_FORCE_UPDATE', true);
+
+    if (is_admin()) { // note the use of is_admin() to double check that this is happening in the admin
+
+        $config = array(
+            'slug' => plugin_basename(__FILE__),
+            'proper_folder_name' => 'he-thong-seo',
+            'api_url' => 'https://api.github.com/repos/duylddev/he-thong-seo-plugin',
+            'raw_url' => 'https://raw.github.com/duylddev/he-thong-seo-plugin/main',
+            'github_url' => 'https://github.com/duylddev/he-thong-seo-plugin',
+            // 'zip_url' => 'https://github.com/duylddev/he-thong-seo-plugin/archive/refs/heads/main.zip',
+            'sslverify' => true,
+            'requires' => '3.0',
+            'tested' => '3.3',
+            'readme' => 'README.md',
+            'access_token' => '',
+        );
+
+        new WP_GitHub_Updater($config);
+
+    }
+
+}
+
+
 // Replace feature image with custom URL from post metadata
 function image_proxy_replace_featured_image($html, $post_id, $post_thumbnail_id, $size, $attr)
 {
